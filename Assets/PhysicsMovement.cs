ffa37867d01da62b9d1e999408d6e22fc0e0de3b
Erik Norstedt <eriknorth.swe@gmail.com>
2020-10-16 16:35:30 +0200

    new scene trying out physics

---
+++ Assets/PhysicsMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed;
+    private Vector3 moveDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        float yStore = moveDirection.y;
+        moveDirection = (transform.forward * Input.GetAxisRaw("Vertical") + transform.right * Input.GetAxisRaw("Horizontal"));
+        
+        moveDirection.y = yStore;
+
+        rb.velocity = new Vector3(moveDirection.x, rb.velocity.y, moveDirection.z).normalized * speed* Time.deltaTime;
+        //rb.velocity = rb.velocity.normalized;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            
+        }
+
+    }
+}
---
+++ Assets/Scripts/ShootingScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingScript : MonoBehaviour
+{
+    public Transform shootPoint;
+    public GameObject bullet;
+
+    public float fireRate = 15f;
+    private float nextTimeFire;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+            if (Input.GetMouseButton(0) && Time.time >= nextTimeFire)
+            {
+                Instantiate(bullet, shootPoint.position, shootPoint.rotation);
+            nextTimeFire = Time.time + 1f / fireRate;
+            }
+       
+    }
+}
