68b4a58d8b93fab53989606cdd6167d198b60c44
Erik Norstedt <eriknorth.swe@gmail.com>
2020-10-20 20:30:03 +0200

    help

--- Assets/PhysicsMovement.cs
+++ Assets/PhysicsMovement.cs
@@ -7,12 +7,23 @@ public class PhysicsMovement : MonoBehaviour
     public Rigidbody rb;
     public float speed;
     private Vector3 moveDirection;
+    public float jumpForce;
+
+    private bool shouldJump;
+
+    public float jumpRememberPressedTime;
+    private float jumpRememberPressed = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
+    void Update()
+    {
+        
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
@@ -23,12 +34,25 @@ public class PhysicsMovement : MonoBehaviour
         moveDirection.y = yStore;
 
         rb.velocity = new Vector3(moveDirection.x, rb.velocity.y, moveDirection.z).normalized * speed* Time.deltaTime;
-        //rb.velocity = rb.velocity.normalized;
+        rb.velocity = rb.velocity.normalized;
 
+        jumpRememberPressed -= Time.deltaTime;
         if (Input.GetButtonDown("Jump"))
         {
-            
+            rb.AddForce(Vector3.up * jumpForce);
+            //jumpRememberPressed = jumpRememberPressedTime;
         }
+
+     /*   if (rb)
+        {
+            //moveDirection.y = 0f;
+            if (jumpRememberPressed > 0f)
+            {
+                jumpRememberPressed = 0f;
+                moveDirection.y = jumpForce;
+            }
+
+        }*/
 
     }
 }
